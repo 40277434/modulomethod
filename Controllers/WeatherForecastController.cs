9baa6d2f2698aacfbfc384e2c833afcad7f3fc9e
40277434 <zwei06@qub.ac.uk>
2021-03-28 02:44:40 +0100

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("/")]
+     
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+         
+        [HttpGet]
+        public string api(string x, string y)        {
+            ResultData r = new ResultData();
+            try
+            {
+                
+                int result = Convert.ToInt32(x) % Convert.ToInt32(y);
+                // Cannot set the key to "string" as it's a reserved key
+                r.expression = $"{x}%{y}";
+                r.answer = $"{result}";
+                r.error=false;
+                
+                
+            }
+            catch (Exception ex)
+            {
+                r.expression = $"{x}%{y}";
+                r.error=true;
+            }
+            return JsonConvert.SerializeObject(r);
+
+        }
+    }
+}
---
+++ ResultData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class ResultData
+    {
+        public string expression { get; set; }
+
+        public string answer { get; set; }
+        public Boolean error { get; set; }
+    }
+}
